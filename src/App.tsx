8938d9ce56eb8e1d9bc7ae72eb1e89e3ad8a0349
ju-kkim <wndus8198@naver.com>
2022-10-13 17:24:13 +0900

    chore: React with TypeScript 환경설정

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App({ textColor }: { textColor: string }) {
+  return <h1 style={{ color: textColor }}>React - TypeScript</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+};
