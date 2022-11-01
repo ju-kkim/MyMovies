cbf79616c14f384af7e787f71f55daf01bf5c52a
ju-kkim <wndus8198@naver.com>
2022-11-01 18:54:26 +0900

    feat: constants 추가
    - GNB Menu
    - error message

---
+++ src/constants/menu.ts
@@ -0,0 +1,20 @@
+const MENU = [
+  {
+    text: '인기영화',
+    path: 'popular',
+  },
+  {
+    text: '현재상영중',
+    path: 'now_playing',
+  },
+  {
+    text: '개봉예정',
+    path: 'upcoming',
+  },
+  {
+    text: '높은평점',
+    path: 'top_rated',
+  },
+];
+
+export default MENU;
