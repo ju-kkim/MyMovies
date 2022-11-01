d32257360dd2b5cee14dd4333e196cd14f547f4c
ju-kkim <wndus8198@naver.com>
2022-11-01 19:04:50 +0900

    feat: App route, cookieProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import Header from '@/layout/Header';
+import Home from '@/page/Home';
+import Login from '@/page/Login';
+import NotFound from '@/page/NotFound';
+import { getCookie } from './utils/cookie';
+import { getAccount } from './utils/api/account';
+import { useSetRecoilState } from 'recoil';
+import { userStore } from './store/user';
 
 export default function App() {
-  return <h1>React - TypeScript</h1>;
+  const setUserStore = useSetRecoilState(userStore);
+
+  useEffect(() => {
+    if (!getCookie('sessionId')) return;
+    (async () => {
+      const user = await getAccount();
+      setUserStore(user);
+    })();
+  }, []);
+
+  return (
+    <CookiesProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
+  );
 }
