6dd4cecd063364759d0e8e9a69420b8d4214da69
ju-kkim <wndus8198@naver.com>
2022-11-08 22:11:59 +0900

    fix: 로그인 기능 수정
    - useRecoilValueLoadable 활용

--- src/components/LoginFrom/index.tsx
+++ src/components/LoginFrom/index.tsx
@@ -1,45 +1,40 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValueLoadable } from 'recoil';
 import styled from 'styled-components';
 import COLOR from '@/common/color';
 import { typography } from '@/common/mixins';
-import { createSessionId } from '@/utils/api/authentication';
-import { getAccount } from '@/utils/api/account';
 import { userStore } from '@/store/user';
+import { useAuth } from '@/hook/useAuth';
+import { NAVIGATE } from '@/constants/navigate';
 
 export default function LoginFrom() {
-  const [isLoginFailed, setIsLoginFailed] = useState(false);
-  const setUserStore = useSetRecoilState(userStore);
-
   const navigate = useNavigate();
+  const { createSessionId, errorMessage } = useAuth();
+  const user = useRecoilValueLoadable(userStore);
   const userID = useRef<HTMLInputElement | null>(null);
   const userPW = useRef<HTMLInputElement | null>(null);
 
-  async function onLogin(e: React.MouseEvent) {
+  useEffect(() => {
+    if (user.state !== 'hasValue') return;
+    if (user.contents.isLogin) navigate(NAVIGATE.BACK);
+  }, [user]);
+
+  function onLogin(e: React.MouseEvent) {
     e.preventDefault();
     const username = userID.current?.value;
     const password = userPW.current?.value;
 
-    try {
-      await createSessionId({ username, password });
-      const user = await getAccount();
-      setUserStore(user);
-      setIsLoginFailed(false);
-      navigate(-1);
-    } catch (error) {
-      setIsLoginFailed(true);
-    }
+    createSessionId({ username, password });
   }
 
   return (
     <>
-      {isLoginFailed ? (
-        <FailedText>로그인 정보를 확인하고 다시 시도해 주세요</FailedText>
+      {errorMessage ? (
+        <FailedText>{errorMessage}</FailedText>
       ) : (
         <Description>TMDB 계정으로 사용하실 수 있습니다</Description>
       )}
-
       <LoginFromWrap>
         <LoginInput type="text" placeholder="아이디" ref={userID} />
         <LoginInput type="password" placeholder="비밀번호" ref={userPW} />
---
+++ src/constants/navigate.ts
@@ -0,0 +1,3 @@
+export const NAVIGATE = {
+  BACK: -1,
+};
