9a8671bd7a2c264d5f5945a0121ea402d8d87e23
ju-kkim <wndus8198@naver.com>
2022-11-08 22:59:44 +0900

    fix: 불필요 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import Header from '@/layout/Header/index';
 import Home from '@/page/Home';
 import Login from '@/page/Login';
 import NotFound from '@/page/NotFound';
-import { getCookie } from './utils/cookie';
-import { getAccount } from './utils/api/account';
-import { useSetRecoilState } from 'recoil';
-import { userStore } from './store/user';
 
 export default function App() {
-  const setUserStore = useSetRecoilState(userStore);
-
-  useEffect(() => {
-    if (!getCookie('sessionId')) return;
-    (async () => {
-      const user = await getAccount();
-      setUserStore(user);
-    })();
-  }, []);
-
   return (
     <CookiesProvider>
       <BrowserRouter>
