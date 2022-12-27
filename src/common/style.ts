20119642a2adf77ca4c2673da4e7a84bd1e1ff28
ju-kkim <wndus8198@naver.com>
2022-12-27 16:14:24 +0900

    chore: constants 추가 및 스타일 수정

--- src/common/style.ts
+++ src/common/style.ts
@@ -1,5 +1,6 @@
+import { INNER_PADDING } from '@/constants/constants';
 import styled from 'styled-components';
 
 export const Inner = styled.div`
-  padding: 0 50px;
+  padding: 0 ${INNER_PADDING}px;
 `;
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,5 @@
 export const SESSION_ID = 'sessionId';
+
+export const INNER_PADDING = 50;
+export const PREVIEW_WIDTH = window.innerWidth * 0.3;
+export const DETAIL_WIDTH = window.innerWidth * 0.7;
