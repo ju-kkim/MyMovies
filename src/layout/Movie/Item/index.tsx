f6d3602d6102fbe629fd27b789b0d26a7ad0384e
ju-kkim <wndus8198@naver.com>
2022-12-27 17:25:45 +0900

    feat: Movieitem MouseEvent 추가
    - showPreview
    - canclePreview

--- src/layout/Movie/Item/index.tsx
+++ src/layout/Movie/Item/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
 import { typography } from '@/common/mixins';
 import Image, { imgSize } from '@/components/Image';
 import { movie } from '@/hook/useMovieList';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { modalMovie, modalPosition, modalPositionType } from '@/store/modal';
+import { INNER_PADDING, PREVIEW_WIDTH } from '@/constants/constants';
+
+const MOUSE_TIME = 500;
+const windowWidth = window.innerWidth;
 
 export default function MovieItem({ movie, imgSize }: MovieItem) {
+  const [ModalMovie, setModalMovie] = useRecoilState(modalMovie);
+  const setModalPosition = useSetRecoilState(modalPosition);
+  let isActive = false;
+
+  const showPreview: MouseEventHandler = ({ currentTarget }) => {
+    if (ModalMovie) return;
+    isActive = true;
+    setTimeout(() => {
+      if (!isActive) return;
+      const rects = currentTarget.getBoundingClientRect();
+      const scrolledTopLength = window.pageYOffset;
+      const position = calcPosition({ rects, scrollTop: scrolledTopLength });
+      setModalPosition(position);
+      setModalMovie(movie);
+    }, MOUSE_TIME);
+  };
+
+  const canclePreview = () => (isActive = false);
+
   return (
-    <ItemWrap>
+    <ItemWrap onMouseEnter={showPreview} onMouseLeave={canclePreview}>
       <ImageBox>
         <Image
           type="poster"
@@ -19,6 +44,30 @@ export default function MovieItem({ movie, imgSize }: MovieItem) {
       <Title>{movie.title}</Title>
     </ItemWrap>
   );
+}
+
+function calcPosition({
+  rects,
+  scrollTop,
+}: {
+  rects: DOMRect;
+  scrollTop: number;
+}): modalPositionType {
+  const { x: pointX, y: pointY, width: itemWidth } = rects;
+  const calcY = scrollTop + pointY;
+  const calcX = pointX + itemWidth / 2 - PREVIEW_WIDTH / 2;
+
+  // MovieModal 왼쪽으로 짤렸을 때
+  if (calcX < INNER_PADDING) {
+    return { x: pointX, y: calcY, scrollTop };
+  }
+
+  // MovieModal 오른쪽으로 짤렸을 때
+  if (calcX + PREVIEW_WIDTH >= windowWidth - INNER_PADDING) {
+    return { x: pointX + itemWidth - PREVIEW_WIDTH, y: calcY, scrollTop };
+  }
+
+  return { x: calcX, y: calcY, scrollTop };
 }
 
 const ItemWrap = styled.div`
